9a04eec16ad9df9b87e49b40616d8069db6eaf37
janbuca <janbuca369@gmail.com>
2024-04-25 02:18:33 -0700

    Day 2 can be done

---
+++ Day 2/ch 10.py
@@ -0,0 +1,38 @@
+ # if the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪 
+
+# Write your code below this line 👇 
+
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"));
+tip = int(input("How much tip would you like to given? 10, 12, 15?"));
+
+# people = int(input("How many people to split the bill?"));
+# bill_with_tip = bill * (1 + tip / 100);
+# print(bill_with_tip);
+
+tip_as_percent = tip / 100;
+total_tip_amount = bill * tip_as_percent;
+total_bill = bill + total_tip_amount;
+
+people = int(input("How many people to split the bill?"));
+
+
+bill_per_person = total_bill / people;
+final_amount = round(bill_per_person, 2);
+print(f"Each person should pay: ${final_amount}");
+
+# how to round to 2 decimal places in python "Stack overflow"
+
+# Search for round function in python
+# https://www.w3schools.com/python/ref_func_round.asp
+
+ 
+
+
+
+
