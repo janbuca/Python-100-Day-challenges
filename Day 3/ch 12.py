44ee0bbeec20f08b1bcfaa1cf2ea2cb23cb6237e
janbuca <janbuca369@gmail.com>
2024-04-25 23:53:53 -0700

    Odd or Even Exercise

---
+++ Day 3/ch 12.py
@@ -0,0 +1,7 @@
+# Odd or Even Exercise
+print("Enter any natural number? ");
+num = int(input("The Entering Number is "));
+if (num % 2 == 0):
+    print("This is even Number");
+else:
+    print("This is an Odd Number");
