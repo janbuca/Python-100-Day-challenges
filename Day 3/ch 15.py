9d5cdae27bd96ef85c79a6530503770882f7d0df
janbuca <janbuca369@gmail.com>
2024-04-26 01:46:35 -0700

    Pizza Ordering in Size

--- Day 3/ch 15.py
+++ Day 3/ch 15.py
@@ -1,12 +1,12 @@
 # Pizza Order Exercise
 print("Welcome to Python Pizza Deliveries!") 
-#print ("\tSmall size Pizza: $15 \t\n Medium size pizza: $20 \t\n Large pizza: $25");
+print ("\tSmall size Pizza: $15 \t\t\n Medium size pizza: $20 \t\t\n Large pizza: $25");
 size = input("What Size pizza do you want? S, M, or L:  ")
-#print ("Pepperoni for Small pizza: +$2 \t\n Pepperoni for Medium pizza: $+3" )
+print ("\t\t\tPepperoni for Small pizza: +$2 \t\t\n Pepperoni for Medium pizza: $+3" )
 add_pepperoni = input("Do you want pepperoni? Y or N:  ")
-#print ("Extra Cheese for any size pizza: +$2")
+print ("\t\tExtra Cheese for any size pizza: +$2")
 extra_cheese = input("Do you want extra Cheese? Y, N:  ")  
-    
+
 bill = 0;
 
 if size == "S":
