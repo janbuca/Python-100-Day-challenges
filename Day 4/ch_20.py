cf0d556c0e165739a148f57fcf816dd881a870bb
janbuca <janbuca369@gmail.com>
2024-04-27 01:24:42 -0700

    List are adding

---
+++ Day 4/ch_20.py
@@ -0,0 +1,10 @@
+# List
+states_of_ethiopian = ["Oriya", "Somalia", "Amhara", "Tigre"]
+print(states_of_ethiopian[3]);
+
+states_of_ethiopian.append("Sudan");
+print(states_of_ethiopian);
+
+states_of_ethiopian.extend(["Herr", "Sigma", "SNAP"])
+print(states_of_ethiopian)
+
