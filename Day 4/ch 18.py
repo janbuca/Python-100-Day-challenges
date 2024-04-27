a22cf677972a252036b209dd5246e0dff0090d9b
janbuca <janbuca369@gmail.com>
2024-04-27 01:05:21 -0700

    Some of file can be import

--- Day 4/ch 18.py
+++ Day 4/ch 18.py
@@ -1,4 +1,11 @@
 # Random Module
 import random
+import ch_19
 random_integer = random.randint(1, 10);
+random_float = random.random()
 print(random_integer);
+print(random_float);
+mul = random_float * 5;
+mul = random_float * 5;
+print(mul);
+print(ch_19.pi);
