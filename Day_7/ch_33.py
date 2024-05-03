8cdbc31d71163df8ab8eda2a765f3cee3d8943b3
janbuca <janbuca369@gmail.com>
2024-05-02 22:06:04 -0700

    upDating

--- Day_7/ch_33.py
+++ Day_7/ch_33.py
@@ -4,6 +4,13 @@ word_list = ["ardvark", "baboon", "camel"]
 import random
 chosen_word = random.choice(word_list)
 
+print(f"Pssst, the solution is {chosen_word}.")
+
+display = []
+for letter in chosen_word:
+    display += "_";
+print(display);
+
 guess = input("Guess a letter: ").lower()
 
 for letter in chosen_word:
